e42123d0a39d69b48ceccd3fa79e151e2253c4d3
rahmanagha <aghazad.rahman@gmail.com>
2026-04-19 18:36:54 +0400

    refactor: Extract helper methods in Game for readability

--- lib/game.rb
+++ lib/game.rb
@@ -3,27 +3,49 @@ require_relative "human_player"
 
 
 class Game
-  
+
  def play
   turn = 0
   code = Code.new
   human = HumanPlayer.new
 
   until turn == 12
-    puts "Turn #{turn+1}/12"
+    display_turn(turn)
     guess = human.get_guess
     feedback = code.evaluate(guess)
-    if feedback[:perfect_match] == 4
-      puts "Congratulations! You guessed correctly"
-      puts "The secret was: #{Code.numbers_to_colors(code.secret).join(', ')}"
-      break
-    end
-    puts "Perfect matches: #{feedback[:perfect_match]}, Color matches: #{feedback[:color_match]}"
-     turn+=1
+    break if handle_win(code, feedback)
+    display_feedback(feedback)
+    turn += 1
   end
-  unless feedback[:perfect_match] == 4
-    puts "You've run out of turns. The secret code was: #{Code.numbers_to_colors(code.secret).join(', ')}"
-  end
+
+  handle_loss(code, feedback)
  end
 
+ private
+
+ def display_turn(turn)
+   puts "Turn #{turn+1}/12"
+ end
+
+ def display_feedback(feedback)
+   puts "Perfect matches: #{feedback[:perfect_match]}, Color matches: #{feedback[:color_match]}"
+ end
+
+ def won?(feedback)
+   feedback[:perfect_match] == 4
+ end
+
+ def handle_win(code, feedback)
+   if won?(feedback)
+     puts "Congratulations! You guessed correctly"
+     puts "The secret was: #{Code.numbers_to_colors(code.secret).join(', ')}"
+   end
+ end
+
+ def handle_loss(code, feedback)
+   unless won?(feedback)
+     puts "You've run out of turns."
+     puts "The secret code was: #{Code.numbers_to_colors(code.secret).join(', ')}"
+   end
+ end
 end
