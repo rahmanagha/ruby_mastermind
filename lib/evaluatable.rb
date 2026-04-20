410756ee21571032a4bf740016d5780b078cf2da
rahmanagha <aghazad.rahman@gmail.com>
2026-04-20 15:53:19 +0400

    feat: Add Evaluatable module for shared scoring logic

---
+++ lib/evaluatable.rb
@@ -0,0 +1,45 @@
+module Evaluatable
+  def evaluate(secret, guess)
+    response = {perfect_match: 0, color_match: 0}
+    response[:perfect_match] = count_perfect_matches(secret, guess)
+    edited_guess, edited_secret = remove_perfect_matches(secret, guess)
+    response[:color_match] = count_color_matches(edited_guess, edited_secret)
+    response
+  end
+
+  def count_perfect_matches(secret, guess)
+    matches = 0
+    guess.each_with_index do |number, index|
+      matches+=1 if guess[index] == secret[index]
+    end
+    matches
+  end
+
+  def remove_perfect_matches(secret, guess)
+    edited_guess = edit_array(guess, secret)
+    edited_secret = edit_array(secret, guess)
+    [edited_guess, edited_secret]
+  end
+
+  def count_color_matches(secret, guess)
+    secret_copy = secret.dup
+    matches = 0
+    guess.each_with_index do |number, index|
+      if secret_copy.include?(number)
+        matches+=1
+        delete_index = secret_copy.find_index(number)
+        secret_copy.delete_at(delete_index)
+      end
+    end
+    matches
+  end
+
+  def edit_array(edited_array, base_array)
+    new_array = []
+    edited_array.each_with_index do |number, index|
+      new_array.push(number) if number != base_array[index]
+    end
+    new_array
+  end
+
+end
