17688e1bf6567055a4295a77b408c98be7d45cc0
rahmanagha <aghazad.rahman@gmail.com>
2026-04-21 11:22:35 +0400

    refactor: Simplify HumanPlayer to use InputHandler module

--- lib/human_player.rb
+++ lib/human_player.rb
@@ -1,21 +1,10 @@
-require_relative "validatable"
+require_relative "input_handler"
 
 class HumanPlayer
 
-  include Validatable
-
-  def check_guess(string)
-    converted_array = string.split("").map {|number| number.to_i}
-    return converted_array if valid_input?(converted_array)
-    false
-  end
+  include InputHandler
 
   def get_guess
-    puts "Enter 4 digits (1-6), e.g., 1234"
-    loop do
-      guess = check_guess(gets.chomp)
-      return guess if guess   # Returns the array directly when valid
-      puts "Please enter a valid guess!"
-    end
+    get_input
   end
 end
