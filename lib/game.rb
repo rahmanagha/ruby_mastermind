d797262195968fbd3f9b06d9ca52f7a8437d6045
rahmanagha <aghazad.rahman@gmail.com>
2026-04-21 12:14:37 +0400

    feat: Add dual-mode Game with human and computer guessers

--- lib/game.rb
+++ lib/game.rb
@@ -1,18 +1,30 @@
 require_relative "code"
 require_relative "human_player"
+require_relative "human_coder"
+require_relative "computer_player"
 
 
 class Game
 
-  def play
+  def play(mode)
     turn = 0
-    code = Code.new
-    human = HumanPlayer.new
+
+    if mode == 1
+      code = Code.new
+      human = HumanPlayer.new
+      current_player = human
+    elsif mode == 2
+      code = HumanCoder.new
+      code.get_secret
+      computer = ComputerPlayer.new
+      current_player = computer
+    end
 
     until turn == 12
       display_turn(turn)
-      guess = human.get_guess
+      guess = current_player.get_guess
       feedback = code.evaluate_code(guess)
+      computer.receive_feedback(guess, feedback) if mode == 2
       break if handle_win(code, feedback)
       display_feedback(feedback)
       turn += 1
@@ -37,7 +49,7 @@ class Game
 
   def handle_win(code, feedback)
     if won?(feedback)
-      puts "Congratulations! You guessed correctly"
+      puts "The guesser has won"
       puts "The secret was: #{Code.numbers_to_colors(code.secret).join(', ')}"
       true
     end
@@ -45,7 +57,7 @@ class Game
 
   def handle_loss(code, feedback)
     unless won?(feedback)
-      puts "You've run out of turns."
+      puts "The guesser ran out of turns."
       puts "The secret code was: #{Code.numbers_to_colors(code.secret).join(', ')}"
     end
   end
