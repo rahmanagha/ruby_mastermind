529b82499c0a8fe54c576a2f7cda6a1667c22ac7
rahmanagha <aghazad.rahman@gmail.com>
2026-04-21 11:37:23 +0400

    feat: Add ComputerPlayer with minimax algorithm

---
+++ lib/computer_player.rb
@@ -0,0 +1,46 @@
+class ComputerPlayer
+  
+  include Evaluatable
+
+  def initialize
+    @all_codes = generate_all_codes
+    @possible_codes = @all_codes.dup
+    @last_guess = nil
+  end
+  def get_guess
+    @last_guess = pick_guess
+    @last_guess
+  end
+
+  def pick_guess()
+    return [1,1,2,2] if @last_guess == nil
+    lowest_worst_case = 1296
+    best_guess = nil
+    @possible_codes.each do |possible_guess|
+      possible_responses_count = Hash.new(0)
+      @possible_codes.each do |possible_secret|
+        response = evaluate(possible_secret, possible_guess)
+        possible_responses_count[[response[:perfect_match], response[:color_match]]]+=1
+      end
+
+      worst_case_for_this_guess = possible_responses_count.values.max
+
+      if worst_case_for_this_guess < lowest_worst_case
+        lowest_worst_case = worst_case_for_this_guess
+        best_guess = possible_guess
+      elsif worst_case_for_this_guess == lowest_worst_case
+        best_guess = possible_guess
+      end
+    end
+    best_guess
+  end
+
+  def receive_feedback(guess, feedback)
+    @possible_codes.select! {|possible_code| evaluate(possible_code, guess) == feedback}
+  end
+
+  def generate_all_codes
+    digits = [1, 2, 3, 4, 5, 6]
+    digits.product(digits, digits, digits)
+  end
+end
